71c616b684bc3b6cafee8ff67f66194e91081952
Mauricio Fossas Koval <mauriciofossaskoval@clairesplewatch.dhcp.wustl.edu>
2020-01-30 15:37:31 -0600

    Almost finished studio

--- app/src/main/java/com/example/studio2/ChangeActivity.kt
+++ app/src/main/java/com/example/studio2/ChangeActivity.kt
@@ -2,12 +2,14 @@ package com.example.studio2
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_change.*
 
 class ChangeActivity : AppCompatActivity() {
 
+    var depFrag = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,4 +17,36 @@ class ChangeActivity : AppCompatActivity() {
 
     }
 
+    override fun onStart() {
+        super.onStart()
+        switchFragment(null)
+    }
+
+    fun switchFragment(view: View?){
+        if(depFrag){
+            val withdraw = WithdrawFragment()
+            withdraw.arguments = Bundle()
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.container, withdraw)
+            transaction.commit()
+            depFrag = false
+        } else{
+            val deposit = DepositFragment()
+
+            deposit.arguments = Bundle()
+
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.container, deposit)
+            transaction.commit()
+            depFrag = true
+        }
+    }
+
+    fun deposit(){
+
+    }
+
+    fun withdraw(){
+
+    }
 }
--- app/src/main/java/com/example/studio2/MainActivity.kt
+++ app/src/main/java/com/example/studio2/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.example.studio2
 
+import android.app.Activity
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
+
+    private val CURRENT_BALANCE = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,4 +20,18 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    fun goToChange(view: View){
+        val intentToChange = Intent(this, ChangeActivity::class.java)
+        startActivityForResult(intentToChange, CURRENT_BALANCE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode== CURRENT_BALANCE){
+            if(resultCode== Activity.RESULT_OK){
+                //Do something here
+
+            }
+        }
+    }
 }
